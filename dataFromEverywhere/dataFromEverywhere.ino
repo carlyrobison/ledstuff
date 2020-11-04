3eba993f0c4e4489d398cedce26e2eec6b63b879
carlyrobison <carly.ellen.robison@gmail.com>
2020-11-03 20:32:09 -0800

    Refactored rainbow cycling

--- dataFromEverywhere/dataFromEverywhere.ino
+++ dataFromEverywhere/dataFromEverywhere.ino
@@ -11,13 +11,15 @@ FASTLED_USING_NAMESPACE
 #define COLOR_ORDER GRB
 #define NUM_LEDS    64
 CRGB leds[NUM_LEDS];
+CRGB rainbow[6] = {CRGB::Purple, CRGB::Blue, CRGB::Green, CRGB::Yellow, CRGB::Orange, CRGB::Red};
+int i = 0;
 
-#define BRIGHTNESS          96
+#define BRIGHTNESS          50
 #define FRAMES_PER_SECOND  120
 
 // put your setup code here, to run once:
 void setup() {
-  delay(3000); // 3 second delay for recovery
+  delay(1000); // 1 second delay for recovery
   
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
@@ -27,25 +29,14 @@ void setup() {
 }
 
 void loop() {
-  // Turn the LED on, then pause
+  // Cycle through rainbow
+  cycleRainbow(i);
+  i++;
   
-  leds[0] = CRGB::Purple;
   FastLED.show();
   delay(500);
+}
 
-  leds[0] = CRGB::Blue;
-  FastLED.show();
-  delay(500);
-
-  leds[0] = CRGB::Green;
-  FastLED.show();
-  delay(500);
-
-  leds[0] = CRGB::Yellow;
-  FastLED.show();
-  delay(500);
-
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(500);
+void cycleRainbow(int i) {
+  leds[0] = rainbow[i%6];
 }
