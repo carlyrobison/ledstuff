1b7a7a05dc715303014f851426cb32c456f53b01
carlyrobison <carly.ellen.robison@gmail.com>
2020-11-03 21:21:14 -0800

    Added HTTP client stuff, not sure if that part is working yet

--- dataFromEverywhere/dataFromEverywhere.ino
+++ dataFromEverywhere/dataFromEverywhere.ino
@@ -1,4 +1,13 @@
+#include <ssl_client.h>
+
 #include <FastLED.h>
+//#include <Arduino.h>
+//
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include "secrets.h"
+//#include <WiFi.h>
 
 FASTLED_USING_NAMESPACE
 
@@ -15,26 +24,118 @@ CRGB rainbow[6] = {CRGB::Purple, CRGB::Blue, CRGB::Green, CRGB::Yellow, CRGB::Or
 int i = 0;
 
 #define BRIGHTNESS          50
-#define FRAMES_PER_SECOND  120
+#define FRAMES_PER_SECOND    6
+
+WiFiMulti WiFiMulti;
+// defined in secrets.h
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+
+// Not sure if WiFiClientSecure checks the validity date of the certificate. 
+// Setting clock just to be sure...
+void setClock() {
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print(F("Waiting for NTP time sync: "));
+  time_t nowSecs = time(nullptr);
+  while (nowSecs < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(F("."));
+    yield();
+    nowSecs = time(nullptr);
+  }
+
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&nowSecs, &timeinfo);
+  Serial.print(F("Current time: "));
+  Serial.print(asctime(&timeinfo));
+}
 
 // put your setup code here, to run once:
 void setup() {
-  delay(1000); // 1 second delay for recovery
-  
+  Serial.begin(9600);
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(SECRET_SSID, SECRET_PASS);
+
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
+  leds[0] = CRGB::Red;
+  FastLED.show();
+
+//   wait for WiFi connection
+  Serial.println("Waiting for WiFi to connect...");
+  while ((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.println(".");
+  }
+  Serial.println(" connected");
+  leds[0] = CRGB::Orange;
+  FastLED.show();
+
+//  setClock();
+  leds[0] = CRGB::Green;
+  FastLED.show();
 }
 
 void loop() {
   // Cycle through rainbow
   cycleRainbow(i);
   i++;
+
+//  webLoop();
   
   FastLED.show();
-  delay(500);
+  delay(1000/FRAMES_PER_SECOND);
+}
+
+void webLoop(){
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    client -> setCACert(rootCACertificate);
+
+    {
+      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+      HTTPClient https;
+  
+      Serial.print("[HTTPS] begin...\n");
+      if (https.begin(*client, "https://jigsaw.w3.org/HTTP/connection.html")) {  // HTTPS
+        Serial.print("[HTTPS] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = https.GET();
+  
+        // httpCode will be negative on error
+        if (httpCode > 0) {
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+  
+          // file found at server
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String payload = https.getString();
+            Serial.println(payload);
+          }
+        } else {
+          Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        }
+  
+        https.end();
+      } else {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
+
+      // End extra scoping block
+    }
+  
+    delete client;
+  } else {
+    Serial.println("Unable to create client");
+  }
+
+  Serial.println();
+  Serial.println("Waiting 1s before the next round...");
+  delay(1000);
 }
 
 void cycleRainbow(int i) {
