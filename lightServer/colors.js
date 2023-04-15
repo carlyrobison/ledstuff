ebeed5dc28bff950a88bf90f37475e380ddadf9d
Carly Robison <carlyellenrobison@gmail.com>
2023-04-14 22:12:44 -0500

    Add colors from forecast points

--- lightServer/colors.js
+++ lightServer/colors.js
@@ -20,10 +20,28 @@ function peopleToHSV(number) {
     return [hue, 100, 100];
 }
 
+// This looks bad, but it's because it's descriptively taken from https://www.weather.gov/forecastpoints
+function tempFtoRGB(number) {
+  if (number >= 65) { return [100, 100, 100] }
+  if (number >= 60) { return [161, 217, 155] }  // light green
+  if (number >= 55) { return [116, 196, 118] }  // darker green
+  if (number >= 50) { return [49, 163, 84] }    // deeper green
+  if (number >= 45) { return [100, 100, 100] }
+  if (number >= 40) { return [18, 87, 87] }  // grayish blue
+  if (number >= 35) { return [0, 153, 150] }
+  if (number >= 30) { return [48, 207, 194] } 
+  if (number >= 25) { return [173, 255, 255] }
+  if (number >= 20) { return [115, 215, 255] } 
+  if (number >= 15) { return [74, 199, 255] }
+  return [100, 100, 100];
+}
+
 function numberToHSV(data, number) {
     switch(data) {
         case 'PeopleInSpace':
             return peopleToHSV(number);
+        case 'TempF':
+          return tempFtoRGB(number);
         default:
             return [0, 0, 0];
     }
