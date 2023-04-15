894b27233908e0a561c19402b4d2e7c0ed4868da
Carly Robison <carlyellenrobison@gmail.com>
2023-04-14 21:43:47 -0500

    Improved forecast endpoint

--- lightServer/forecastpoints.js
+++ lightServer/forecastpoints.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch');
 const secrets = require('./secrets.js');
 
-const HUE_MAX = 290;
-
 // FORECASTPOINTS: the government gives pretty granulat weather data!
 // https://api.weather.gov/points/lat,long
+// other endpoints: /offices/{officeId}/headlines'
 
 async function forecast() {
 
   let res = await fetch('https://api.weather.gov/gridpoints/' + secrets.gridpointCode + '/forecast/hourly')
+    .then(res => res.json());
 
   console.log(res)
 
